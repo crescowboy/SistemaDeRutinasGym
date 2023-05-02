02001c08d8fda70581b6cbf2befd733aafb74acc
wcrespo <wellingtoncrespo2001@gmail.com>
2023-05-01 21:59:55 -0500

    form marcas pr

--- src/paginas/RegistrarMarcas.jsx
+++ src/paginas/RegistrarMarcas.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 
 const RegistrarMarcas = () => {
+  
   return (
-    <div>
-      registrar
-    </div>
+    <>
+
+      <form >
+
+      
+
+      <div className='container-marcas'>
+        <div className='content-marcas'>
+        <h1>Marcas registradas:</h1>
+        <div className='input-marcas'>
+        <label htmlFor="">Press Banca:</label>
+        <input type="text" />
+        <span className='kg'>kg</span> 
+        </div>
+        <div className='input-marcas'>
+        <label htmlFor="">Sentadilla:</label>
+        <input type="text" />
+        <span className='kg'>kg</span> 
+        </div>
+        <div className='input-marcas'>
+        <label htmlFor="">Peso muerto:</label>
+        <input type="text" />
+        <span className='kg'>kg</span> 
+        </div>
+        </div>
+      </div>
+
+      </form>
+    </>
   )
 }
 
