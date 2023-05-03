4b0ef978fb9a9ca90178b857862283c9ba2a4ae5
wcrespo <wellingtoncrespo2001@gmail.com>
2023-05-03 13:34:00 -0500

    cambios2

--- src/paginas/RegistrarMarcas.jsx
+++ src/paginas/RegistrarMarcas.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import Pr from './Pr';
+
 
 
 
@@ -25,27 +25,29 @@ const RegistrarMarcas = () => {
 
       <div className='container-marcas'>
         <div className='content-marcas'>
-        <h1>Marcas registradas:</h1>
+          <h1>Marcas registradas:</h1>
+          <div className='input-marcas'>
+            <label htmlFor="banca">Press Banca:</label>
+            <input type="text" id='banca' 
+            {...register("banca")}
+            />
+            <span className='kg'>kg</span> 
+          </div>
+
         <div className='input-marcas'>
-        <label htmlFor="banca">Press Banca:</label>
-        <input type="text" id='banca' 
-        {...register("banca")}
-        />
-        <span className='kg'>kg</span> 
+          <label htmlFor="sentadilla">Sentadilla:</label>
+          <input type="text" id='sentadilla'
+          {...register("sentadilla")}
+          />
+          <span className='kg'>kg</span> 
         </div>
+
         <div className='input-marcas'>
-        <label htmlFor="sentadilla">Sentadilla:</label>
-        <input type="text" id='sentadilla'
-        {...register("sentadilla")}
-        />
-        <span className='kg'>kg</span> 
-        </div>
-        <div className='input-marcas'>
-        <label htmlFor="pesoMuerto">Peso muerto:</label>
-        <input type="text" id='pesoMuerto'
-        {...register("pesoMuerto")}
-        />
-        <span className='kg'>kg</span> 
+          <label htmlFor="pesoMuerto">Peso muerto:</label>
+          <input type="text" id='pesoMuerto'
+          {...register("pesoMuerto")}
+          />
+          <span className='kg'>kg</span> 
         </div>
 
         <div className='boton-marcas'>
