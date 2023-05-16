b07eb7e8e9026046f1d4ea094edeb9efc4ad2afb
wcrespo <wellingtoncrespo2001@gmail.com>
2023-05-15 22:59:45 -0500

    cambios

--- src/paginas/GuardarRutina.jsx
+++ src/paginas/GuardarRutina.jsx
@@ -1,71 +1,65 @@
-import React, { useContext, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import {Contexto} from '../context/Contexto';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Contexto } from '../context/Contexto';
 
 const GuardarRutina = () => {
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { cancelarNombreRutina, setCancelarNombreRutina, agregarEjercicio, setAgregarEjercicio, misRutinas, setMisRutinas } = useContext(Contexto);
+  const { nombreRutina, setNombreRutina } = useContext(Contexto);
 
-    const {register,handleSubmit,reset, formState:{errors}} = useForm();
-    const {cancelarNombreRutina,setCancelarNombreRutina,
-           agregarEjercicio, setAgregarEjercicio} = useContext(Contexto);
-    const {nombreRutina,setNombreRutina,misRutinas,setMisRutinas} = useContext(Contexto);
-    
-    const cancelar =()=>{
-        setCancelarNombreRutina(false)
-        setAgregarEjercicio(false)
-        reset()
-    }
-    
-    const obtenerValores =(data)=>{
-        console.table(data)
-        setAgregarEjercicio(true)
+  const cancelar = () => {
+    setCancelarNombreRutina(false);
+    setAgregarEjercicio(false);
+    reset();
+  };
 
-        setNombreRutina([...nombreRutina,
-        {
-            nombre: data.nombre
-        }
-        ])
+  const obtenerValores = (data, index) => {
+    console.table(data);
+    setAgregarEjercicio(true);
 
-        setMisRutinas([...misRutinas,
-        {
-            nombre: data.nombre
-        }
-        ])
-        reset()
-        
-    }
+    const objetoActualizado = { ...misRutinas[index], nombre: data.nombre };
+    const misRutinasActualizadas = [...misRutinas];
+    misRutinasActualizadas[index] = objetoActualizado;
+    setMisRutinas(misRutinasActualizadas);
+
+    // setMisRutinas([
+    //   ...misRutinas,
+    //   {
+    //     nombre: data.ejercicio
+    //   }
+    // ]);
+    reset();
+  };
+
+  const indice = 0;
 
   return (
     <>
-
-    {
-        cancelarNombreRutina===true
-        ?
-        <div className='tabla-nueva-rutina'>      
-        <form onSubmit={handleSubmit(obtenerValores)}>
-            <label htmlFor="nombre">Nombre de rutina: </label>
-            <input type="text" id='nombre'
-            {
-                ...register('nombre',
-                {
-                    required: 'Este campo es requerido'
-                })
-            }
+      {cancelarNombreRutina === true ? (
+        <div className='tabla-nueva-rutina'>
+          <form onSubmit={handleSubmit((data) => obtenerValores(data, indice))}>
+            <label htmlFor='nombre'>Nombre de rutina: </label>
+            <input
+              type='text'
+              id='nombre'
+              {...register('nombre', {
+                required: 'Este campo es requerido'
+              })}
             />
-
             {errors.nombre && <div className='error'>{errors.nombre.message}</div>}
             <div className='botones-nueva-rutina'>
-            <button className='boton-rutinas' type='submit'>Guardar</button>
-            <button className='boton-rutinas' type='button' onClick={cancelar}>Cancelar</button>
+              <button className='boton-rutinas' type='submit'>
+                Guardar
+              </button>
+              <button className='boton-rutinas' type='button' onClick={cancelar}>
+                Cancelar
+              </button>
             </div>
-        </form>
+          </form>
         </div>
-
-        :
-        null
-    }
-        
+      ) : null}
     </>
-  )
-}
+  );
+};
 
-export default GuardarRutina
+export default GuardarRutina;
--- src/paginas/MisRutinas.jsx
+++ src/paginas/MisRutinas.jsx
@@ -3,45 +3,40 @@ import Nav from '../Nav'
 import { Contexto } from '../context/Contexto'
 
 const MisRutinas = () => {
-  const { misRutinas, setMisRutinas, objetoMisRutinas, setObjetoMisRutinas, nombreRutina, setNombreRutina } = useContext(Contexto);
+  const { misRutinas, setMisRutinas, objetoMisRutinas, setObjetoMisRutinas, nombreRutina, setNombreRutina,agregarEjercicio, setAgregarEjercicio } = useContext(Contexto);
+
+  
 
   return (
     <>
       <div className='container-misRutinas-padre'>
-        <Nav></Nav>
+        <Nav />
         <div className='container-misRutinas-hijo'>
           <h1>Mis rutinas: </h1>
-          <div className='tabla-misRutinas'>
-           
-                
-                 
-                  {misRutinas.map((rutina,index)=>(
-                    <div className="fila-misRutinas" key={index}>
 
-                      {rutina.ejercicio &&(
-                        <>
-                        <h2>{rutina.nombre}</h2>
-                        <li>
-                          Ejercicio: {rutina.ejercicio}, Series: {rutina.series}, Repeticiones: {rutina.repeticiones}
-                        </li>
-                        <button className='boton-eliminar'>eliminar</button>
-                        <button className='boton-editar'>Editar</button>
-                        </>
-                      )
-                        
-                      }
-                      
-                    </div>
-                  ))
-                    
-                  }
-                
-             
-          </div>
+          {misRutinas.length > 0 ? (
+            <div className='tabla-misRutinas'>
+              {misRutinas.map((rutina, index) => (
+                <div className='fila-misRutinas' key={index}>
+                  {rutina.ejercicio && rutina.nombre && (
+                    <>
+                      <h2>{rutina.nombre}</h2>
+                      <li>
+                        Ejercicio: {rutina.ejercicio}, Series: {rutina.series}, Repeticiones: {rutina.repeticiones}
+                      </li>
+                      <button className='boton-eliminar'>eliminar</button>
+                      <button className='boton-editar'>agregar</button>
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p>No hay rutinas disponibles</p>
+          )}
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 export default MisRutinas
