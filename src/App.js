2208e4a0c5305a6f23fa9e9ae85756a77b654207
wcrespo <wellingtoncrespo2001@gmail.com>
2023-05-21 21:47:55 -0500

    componente Guia Ejercicios y creacion de ruta

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RegistrarMarcas from "./paginas/RegistrarMarcas";
 import RegistrarRutina from "./paginas/RegistrarRutina";
 import Provider from "./context/Provider";
 import MisRutinas from "./paginas/MisRutinas";
+import GuiaEjercicios from "./paginas/GuiaEjercicios";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path='/registrarPr' element={<RegistrarMarcas></RegistrarMarcas>}></Route>
         <Route path="/registrarRutinas" element={<RegistrarRutina></RegistrarRutina>}></Route>
         <Route path="/misRutinas" element={<MisRutinas></MisRutinas>}></Route>
+        <Route path="/guiaEjercicios" element={<GuiaEjercicios></GuiaEjercicios>}></Route>
     </Routes>
     </Provider>
     </>
---
+++ src/paginas/GuiaEjercicios.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuiaEjercicios = () => {
+  return (
+    <>
+      Guia Ejercicios
+    </>
+  )
+}
+
+export default GuiaEjercicios
