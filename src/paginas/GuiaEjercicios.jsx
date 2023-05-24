5920a6370eba5de9d19e62431aac7e64b74e9c10
wcrespo <wellingtoncrespo2001@gmail.com>
2023-05-23 23:08:44 -0500

    estilos cards e imagenes

--- src/paginas/GuiaEjercicios.jsx
+++ src/paginas/GuiaEjercicios.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Nav from '../Nav'
+import gym1 from '../img/gym1.jpg'
+import gym2 from '../img/gym2.jpg'
+import gym3 from '../img/gym3.jpg'
 
 const GuiaEjercicios = () => {
   return (
@@ -11,14 +14,17 @@ const GuiaEjercicios = () => {
     <div className='container-card'>
       <div className="card">
         <h3>ganar musculo</h3>
+        <img className='img-card' src={gym1} alt="asd" />
         <button className='btn-ver'>Ver mas</button>
       </div>
       <div className="card">
       <h3>Perder garsa</h3>
+      <img className='img-card' src={gym2} alt="" />
       <button className='btn-ver'>Ver mas</button>
       </div>
       <div className="card">
       <h3>Ganar Resistencia</h3>
+      <img className='img-card' src={gym3} alt="" />
       <button className='btn-ver'>Ver mas</button>
       </div>
     </div>
