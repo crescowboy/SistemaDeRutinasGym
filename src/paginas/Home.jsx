bae333aaf11ddb2728027cbbcf05ee1bca267bb6
wcrespo <wellingtoncrespo2001@gmail.com>
2023-06-12 22:55:23 -0500

    validacion usuario y contraseña

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
-    const navegacion = useNavigate();
+  const handleLogin = () => {
+    // Comparar usuario y contraseña
+    if (user === 'user' && password === 'password') {
+      // Redirigir a la otra página
+      navigate('/registrarPr');
+    } else {
+      // Mostrar mensaje de error o realizar otra acción
+      console.log('Usuario o contraseña incorrectos');
+    }
+  };
+
   return (
     <>
-    <div className='container-empezar'>
-    <div className='empezar'>
-      <h1>Empieza a conseguir resultados!</h1>
-      <div className='login'>
-        <h3>Login</h3>
-        <label htmlFor="user"></label>
-        <input type="text" name='user' />
-        <label htmlFor="password"></label>
-        <input type="text" name='password'/>
-        <button type='submit'>Entrar</button>
+      <div className="container-empezar">
+        <div className="empezar">
+          <h1>Empieza a conseguir resultados!</h1>
+          <div className="login">
+            <h3>Login</h3>
+            <label htmlFor="user">Usuario:</label>
+            <input type="text" name="user" value={user} onChange={(e) => setUser(e.target.value)} />
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" onClick={handleLogin}>
+              Entrar
+            </button>
+          </div>
+          <button className="boton-empezar" onClick={() => navigate('/registrarPr')}>
+            Empezar
+          </button>
+        </div>
       </div>
-      
-      <button className='boton-empezar' onClick={()=> navegacion("/registrarPr")}>Empezar</button> 
-    </div>
-    </div>
-    
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
