0af55a517fb77b27072a99a120890819f932292a
wcrespo <wellingtoncrespo2001@gmail.com>
2024-01-29 23:06:38 -0500

    Button menu responsive

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { Contexto } from './context/Contexto';
 
@@ -7,7 +7,12 @@ const Nav = () => {
 
   const navegacion = useNavigate();
   const {deslogearme} = useContext(Contexto);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen)
+  };
 
   const logout = ()=>{
     navegacion('/home',{replace:true})
@@ -15,20 +20,24 @@ const Nav = () => {
   }
 
   return (
-    <nav className='navbar'>
-        <ul>
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+        <button className="menu-button" onClick={toggleMenu}>
+        ☰
+      </button>
+        <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
           
         <li><NavLink to="/">Inicio</NavLink></li>
         <li><NavLink to="/registrarPr">Registrar Pr</NavLink></li>
-        <li><NavLink to="/registrarRutinas">Registrar runinas</NavLink></li>   
+        <li><NavLink to="/registrarRutinas">Registrar rutinas</NavLink></li>   
         <li><NavLink to="/misRutinas">Mis Rutinas</NavLink></li>
         <li><NavLink to="/guiaEjercicios">Guia</NavLink></li>
           
+        
+        <li> <button onClick={logout}>Salir</button></li> 
+        
         </ul>
 
-        <ul>
-        <li> <button onClick={logout}>Salir</button></li> 
-        </ul>
+        
     </nav>
   )
 }
